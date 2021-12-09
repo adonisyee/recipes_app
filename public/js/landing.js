e8eeea49adbe177724ef1586d12a00756c5f78f3
Adonis Yee <adonisyee@berkeley.edu>
2021-12-09 07:49:25 +0000

    customize landing page and navbar

--- public/js/landing.js
+++ public/js/landing.js
@@ -5,6 +5,9 @@ async function typeText() {
 		i++;
 		setTimeout(typeText, speed);
 	} else {
+		landing_intro.style.transition = "opacity 2s";
+		landing_intro.style.setProperty("-webkit-transition", "opacity 2s");
+		landing_intro.style.setProperty("-moz-transition", "opacity 2s");
 		document.getElementById('landing_intro').style.opacity = '1';
 	}
 }
@@ -13,6 +16,7 @@ async function typeText() {
 let i = 0;
 let title = "Grandma's Recipes";
 let speed = 100;
+let landing_intro = document.getElementById('landing_intro');
 
 //Event Listener
 window.addEventListener('load', typeText);
