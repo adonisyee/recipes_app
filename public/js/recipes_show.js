3566fef0dc378c403219ede091c738ec65378acc
Adonis Yee <adonisyee@berkeley.edu>
2021-12-10 23:18:52 +0000

    conditionally render vote buttons and update voting js

--- public/js/recipes_show.js
+++ public/js/recipes_show.js
@@ -1,15 +1,17 @@
-//Init variables
-const upvoteBtn = document.getElementById('upvote_btn');
-const downvoteBtn = document.getElementById('downvote_btn');
-
-//Event Listeners
-upvoteBtn.addEventListener('click', async function() {
+//Helper functions 
+const sendVote = async (voteType) => {
 	const options = {
 		method: "POST",
 		headers: {
 			'content-Type': 'application/json'
 		},
-		body: JSON.stringify({vote: 'up'})
+	}
+	if (voteType === "up") {
+		options.body = JSON.stringify({vote: 'up'});
+	} else if (voteType === "down") {
+		options.body = JSON.stringify({vote: 'down'});
+	} else {
+		throw("voteType must be 'up' or 'down'");
 	}
 	await fetch("/recipes/vote", options)
 	.then(data => {
@@ -21,4 +23,16 @@ upvoteBtn.addEventListener('click', async function() {
 	.catch(err => {
 		console.log(err);
 	})
+}
+
+//Init variables
+const upvoteBtn = document.getElementById('upvote_btn');
+const downvoteBtn = document.getElementById('downvote_btn');
+
+//Event Listeners
+upvoteBtn.addEventListener('click', async function() {
+	sendVote('up');
+})
+downvoteBtn.addEventListener('click', async function() {
+	sendVote('down');
 })
